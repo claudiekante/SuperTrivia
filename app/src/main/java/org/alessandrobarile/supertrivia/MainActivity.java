f78bc07eb2ded842eb3d5aeeea956d43ba0cfbeb
claudiekante <claudiekante@gmail.com>
2022-11-15 12:44:22 +0100

    listeners

--- app/src/main/java/org/alessandrobarile/supertrivia/MainActivity.java
+++ app/src/main/java/org/alessandrobarile/supertrivia/MainActivity.java
@@ -2,13 +2,62 @@ package org.alessandrobarile.supertrivia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+//    ici il faut brancher les element graphique
+    // avec la methode findViewById() dans OnCreate
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingTextView = findViewById(R.id.main_textview_welcome);
+        mNameEditText = findViewById(R.id.main_edittext_yourname);
+        mPlayButton = findViewById(R.id.main_button_play);
+
+        //ora disattivo il bottone play
+        mPlayButton.setEnabled(false);
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mPlayButton.setEnabled(!editable.toString().isEmpty());
+            }
+        });
+
+        //ora verifico il click sul bottone
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
     }
 }
